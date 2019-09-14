c16caf87f09eee076ef6ecc7eb66cadf45d970d9
Rev Laptop <robotxbp@gmail.com>
2019-09-14 11:56:03 +0300

    commented the TeleOP_Chassis class

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/ConceptVuforiaSkyStoneNavigationWebcam.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/ConceptVuforiaSkyStoneNavigationWebcam.java
@@ -83,7 +83,6 @@ import static org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocaliz
  */
 
 @TeleOp(name="SKYSTONE Vuforia Nav Webcam", group ="Concept")
-@Disabled
 public class ConceptVuforiaSkyStoneNavigationWebcam extends LinearOpMode {
 
     // IMPORTANT: If you are using a USB WebCam, you must select CAMERA_CHOICE = BACK; and PHONE_IS_PORTRAIT = false;
@@ -103,7 +102,7 @@ public class ConceptVuforiaSkyStoneNavigationWebcam extends LinearOpMode {
      * and paste it in to your code on the next line, between the double quotes.
      */
     private static final String VUFORIA_KEY =
-            " --- YOUR NEW VUFORIA KEY GOES HERE  --- ";
+            "AdjawcL/////AAABmQQ4M8HCqEFzongEfPg9UXiMJYRfpEhvImmOHiXl3abvTUgX7AYXfPZGJXWoXsb02N28ekE5l4y1+HVCkJHYZepChGLxEGzzq1zM6AWAcWn3DkJlK/9HIyOiD+CEpWh7R0Bbqwje/zRJfAichvHdSInvwjPQkt/TN2cTEXc0rKuI2lzp6gwkn4G4gjx4n0bD71/C8CW9yNdMycbLDbUcTv5sM4a7TQiqcuPtjGJ96kfEa1z6uvR+DfI89VTqqnL140fXF9IrH9fIg+kSMBHO82qcq4+ZY32bCqTmXUjL6je9rg5Y3J4zMPmic9ACv+1/+zd40lvB33SNjlYjaK/9hinPjUFFNhBiJ5/l3N5EJ/uo";
 
     // Since ImageTarget trackables use mm to specifiy their dimensions, we must use mm for all the physical dimension.
     // We will define some constants and conversions here
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOP_Chassis.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOP_Chassis.java
@@ -10,41 +10,55 @@ import static java.lang.Math.abs;
 @com.qualcomm.robotcore.eventloop.opmode.TeleOp
 
 public class TeleOP_Chassis extends OpMode {
+    /**declare the motors*/
     private DcMotorEx motordf;
     private DcMotorEx motorsf;
     private DcMotorEx motords;
     private DcMotorEx motorss;
+    /**variable for changing the movement speed of the robot*/
     private int v = 2;
+    /**variables for calculating the power for motors*/
     private double df;
     private double sf;
     private double ds;
     private double ss;
     private double max;
-    private double forward, rright, clockwise;
+    /**variables for holding the gamepad joystick values;
+     * we don't want to access them too many times in a loop */
+    private double forward, right, clockwise;
+    /**variable that stops the threads when programs stop*/
     private boolean stop;
+    /**variables that count the thread's fps*/
     private long fpsC=0;
-    private long sysTimeC;
     private long fpsCLast;
+    /** variable that  holds the systime curent time milis*/
+    private long sysTimeC;
+
 
     private Thread Chassis = new Thread( new Runnable() {
         @Override
         public void run() {
+            /**repeat until the program stops*/
             while (!stop) {
+                /**change the variable that controls the speed of the chassis using the bumpers*/
                 if (gamepad1.right_bumper) {
                     v = 1;
                 } else if (gamepad1.left_bumper) {
                     v = 2;
                 }
+                /**getting the gamepad joystick values*/
                 forward = gamepad1.left_stick_y;
-                rright = -gamepad1.left_stick_x;
+                right = -gamepad1.left_stick_x;
                 clockwise = gamepad1.right_stick_x;
 
-                df = forward + clockwise - rright;
-                ss = forward - clockwise - rright;
-                sf = -forward + clockwise - rright;
-                ds = -forward - clockwise - rright;
-                max = abs(sf);
+                /**calculating the power for motors */
+                df = forward + clockwise - right;
+                ss = forward - clockwise - right;
+                sf = -forward + clockwise - right;
+                ds = -forward - clockwise - right;
 
+                /**normalising the power values*/
+                max = abs(sf);
                 if (abs(df) > max) {
                     max = abs(df);
                 }
@@ -53,7 +67,6 @@ public class TeleOP_Chassis extends OpMode {
                 }
                 if (abs(ds) > max) {
                     max = abs(ds);
-
                 }
                 if (max > 1) {
                     sf /= max;
@@ -61,13 +74,14 @@ public class TeleOP_Chassis extends OpMode {
                     ss /= max;
                     ds /= max;
                 }
+                /** fps counter */
                 fpsC++;
                 if (sysTimeC + 1000 < System.currentTimeMillis()) {
                     fpsCLast = fpsC;
                     fpsC = 0;
                     sysTimeC = System.currentTimeMillis();
                 }
-
+                /** setting the speed of the chassis*/
                 if (v == 1) {
                     POWER(df / 5, sf / 5, ds / 5, ss / 5);
                 } else if (v == 2) {
@@ -79,6 +93,7 @@ public class TeleOP_Chassis extends OpMode {
 
     @Override
     public void init() {
+        /**initialization motors */
         motordf = hardwareMap.get(DcMotorEx.class, "df");
         motords = hardwareMap.get(DcMotorEx.class, "ds");
         motorsf = hardwareMap.get(DcMotorEx.class, "sf");
@@ -86,6 +101,8 @@ public class TeleOP_Chassis extends OpMode {
 
         motords.setDirection(DcMotorSimple.Direction.REVERSE);
         motorss.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        /**set the mode of the  motors */
 
         motordf.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         motords.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
@@ -97,11 +114,14 @@ public class TeleOP_Chassis extends OpMode {
         motorsf.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         motorss.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
+        /**initialization sysTime */
         sysTimeC = System.currentTimeMillis();
 
+        /**start the thread*/
         Chassis.start();
     }
 
+    /**using the loop function to send the telemetry to the phone*/
     @Override
     public void loop() {
         telemetry.addData("motordf: ", motordf.getCurrentPosition());
@@ -112,8 +132,10 @@ public class TeleOP_Chassis extends OpMode {
         telemetry.update();
     }
 
+    /**using the stop function to stop the threads */
     public void stop(){stop = true;}
 
+    /**the power functon sets the motor's power*/
     public void POWER(double df1, double sf1, double ds1, double ss1){
         motordf.setPower(df1);
         motorss.setPower(ss1);
