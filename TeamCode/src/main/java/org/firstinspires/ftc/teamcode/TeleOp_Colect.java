e447c0be67a8abe6ca6f04a9114c706263dc69bb
Rev Laptop <robotxbp@gmail.com>
2019-09-14 12:27:38 +0300

    commented the TeleOP_Colect class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_Colect.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_Colect.java
@@ -10,43 +10,57 @@ import static java.lang.Math.abs;
 
 @TeleOp
 public class TeleOp_Colect extends OpMode {
+    /**declare the motors*/
     private DcMotorEx motordf;
     private DcMotorEx motorsf;
     private DcMotorEx motords;
     private DcMotorEx motorss;
     private DcMotor  motorColectSt, motorColectDr;
+    /**variable for changing the movement speed of the robot*/
     private int v = 2;
+    /**variables for calculating the power for motors*/
     private double df;
     private double sf;
     private double ds;
     private double ss;
     private double max;
+    /**variables for holding the gamepad joystick values;
+     * we don't want to access them too many times in a loop */
     private double forward, rright, clockwise;
+    /**variable that stops the threads when programs stop*/
     private boolean stop;
+    /**variables that count the thread's fps*/
     private long fpsC=0;
-    private long sysTimeC;
     private long fpsCLast;
+    /** variable that  holds the systime curent time milis*/
+    private long sysTimeC;
+    /** variables that toggle motors colect */
     private boolean apoz = false, alast = true;
 
     private Thread Chassis_Colect = new Thread( new Runnable() {
         @Override
         public void run() {
+            /**repeat until the program stops*/
             while (!stop) {
+                /**change the variable that controls the speed of the chassis using the bumpers*/
                 if (gamepad1.right_bumper) {
                     v = 1;
                 } else if (gamepad1.left_bumper) {
                     v = 2;
                 }
+                /**getting the gamepad joystick values*/
                 forward = gamepad1.left_stick_y;
                 rright = -gamepad1.left_stick_x;
                 clockwise = gamepad1.right_stick_x;
 
+                /**calculating the power for motors*/
                 df = forward + clockwise - rright;
                 ss = forward - clockwise - rright;
                 sf = -forward + clockwise - rright;
                 ds = -forward - clockwise - rright;
-                max = abs(sf);
 
+                /**normalising the power values*/
+                max = abs(sf);
                 if (abs(df) > max) {
                     max = abs(df);
                 }
@@ -63,6 +77,8 @@ public class TeleOp_Colect extends OpMode {
                     ss /= max;
                     ds /= max;
                 }
+
+                /** fps counter */
                 fpsC++;
                 if (sysTimeC + 1000 < System.currentTimeMillis()) {
                     fpsCLast = fpsC;
@@ -70,12 +86,14 @@ public class TeleOp_Colect extends OpMode {
                     sysTimeC = System.currentTimeMillis();
                 }
 
+                /** setting the speed of the chassis*/
                 if (v == 1) {
                     POWER(df / 5, sf / 5, ds / 5, ss / 5);
                 } else if (v == 2) {
                     POWER(df, sf, ds, ss);
                 }
 
+                /**set the collector motors on or off using the toggle*/
                 boolean abut = gamepad1.x;
                 if(alast != abut){
                     if(gamepad1.x) {
@@ -98,6 +116,7 @@ public class TeleOp_Colect extends OpMode {
 
     @Override
     public void init(){
+        /**initialization motors */
         motordf = hardwareMap.get(DcMotorEx.class, "df");
         motords = hardwareMap.get(DcMotorEx.class, "ds");
         motorsf = hardwareMap.get(DcMotorEx.class, "sf");
@@ -109,6 +128,7 @@ public class TeleOp_Colect extends OpMode {
         motords.setDirection(DcMotorSimple.Direction.REVERSE);
         motorss.setDirection(DcMotorSimple.Direction.REVERSE);
 
+        /**set the mode of the  motors */
         motordf.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         motords.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         motorsf.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
@@ -119,12 +139,15 @@ public class TeleOp_Colect extends OpMode {
         motorsf.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         motorss.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
+        /**initialization sysTime */
         sysTimeC = System.currentTimeMillis();
+
+        /**start the thread*/
         Chassis_Colect.start();
 
     }
 
-
+    /**using the loop function to send the telemetry to the phone*/
     @Override
     public void loop(){
         telemetry.addData("motordf: ", motordf.getCurrentPosition());
@@ -135,9 +158,10 @@ public class TeleOp_Colect extends OpMode {
         telemetry.update();
     }
 
-
+    /**using the stop function to stop the threads */
     public void stop(){stop = true;}
 
+    /**the power functon sets the motor's power*/
     public void POWER(double df1, double sf1, double ds1, double ss1){
         motordf.setPower(df1);
         motorss.setPower(ss1);
