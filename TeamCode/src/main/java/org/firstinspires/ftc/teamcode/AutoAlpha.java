fb292b8f2b9fe8b41e90fc75cc6716606f344c36
Rev Laptop <robotxbp@gmail.com>
2019-09-19 08:38:06 +0300

    ....

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoAlpha.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoAlpha.java
@@ -1,7 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-
+@Autonomous
+@Disabled
 public class AutoAlpha extends LinearOpMode {
     HardwareSkybot r = new HardwareSkybot();
     @Override
